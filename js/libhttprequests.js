bb90389edfbf4a400dbdb6ab2d49aa102bbb603b
return-misaPuding <70533713+return-misaPuding@users.noreply.github.com>
2021-10-19 20:57:33 +0200

    Create libhttprequests.js
    Adds some functions which simplify working with XMLHttpRequests

---
+++ js/libhttprequests.js
@@ -0,0 +1,21 @@
+console.log("my http requests library /js/libhttprequests.js started declaring functions")
+function myHttpRequest(myRequestMethod, myRequestPath, myRequestUnknownBoolean = true) {
+var myRequestResponse;
+  /* credit to W3Schools for the following script */
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       myRequestResponse = xhttp.responseText;
+    }
+};
+xhttp.open(myRequestMethod, myRequestPath, myRequestUnknownBoolean);
+xhttp.send();
+return myRequestResponse;
+}
+
+function myGETRequest(myGETRequestPath, myGETRequestUnknownBoolean = true) {
+ var myGETRequestResponse;
+ myGETRequestResponse = myHttpRequest("GET", myGETRequestPath, myGETRequestUnknownBoolean);
+ return myGETRequestResponse;
+}
