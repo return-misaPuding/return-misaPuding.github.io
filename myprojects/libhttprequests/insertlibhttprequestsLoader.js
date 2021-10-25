b24d3d6854d686b476598c1751301f0738c3307c
return-misaPuding <70533713+return-misaPuding@users.noreply.github.com>
2021-10-25 13:19:58 +0200

    Update insertlibhttprequestsLoader.js

--- myprojects/libhttprequests/insertlibhttprequestsLoader.js
+++ myprojects/libhttprequests/insertlibhttprequestsLoader.js
@@ -1,3 +1,5 @@
 var loadlibhttprequests;
 loadlibhttprequests = "<script src=https://return-misaPuding.github.io/js/libhttprequests.js></script>";
-document.getElementById("howtoloadlibhttprequests").innerText = loadlibhttprequests;
+var libhttprequestsLoaderTag;
+libhttprequestsLoaderTag = "howtoloadlibhttprequests";
+document.getElementById(libhttprequestsLoaderTag).innerText = loadlibhttprequests;
