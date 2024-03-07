e59831053de97bf95c1f429528958c8690f359a7
return-misaPuding <70533713+return-misaPuding@users.noreply.github.com>
2024-03-07 22:57:20 +0100

    Delete myprojects/libhttprequests directory
    WHAT THE HELL IS THIS

--- myprojects/libhttprequests/insertlibhttprequestsLoader.js
+++
@@ -1,5 +0,0 @@
-var loadlibhttprequests;
-loadlibhttprequests = "<script src=https://return-misaPuding.github.io/js/libhttprequests.js></script>";
-var libhttprequestsLoaderTag;
-libhttprequestsLoaderTag = "howtoloadlibhttprequests";
-document.getElementById(libhttprequestsLoaderTag).innerText = loadlibhttprequests;
